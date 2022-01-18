4b2c56e25cf9a11232498cd0c859efc229efabb3
Simranjit Singh <singhz.galaxy@gmail.com>
2022-01-18 03:56:48 +0200

    Add search feature
    Implemented the search feature and did some minor touching up in styles and deletion of Search.js component

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,33 +1,45 @@
 import React from 'react'
-import Search from './Search'
 import Breweries from './Breweries'
 import { useState, useEffect } from "react"
 import axios from 'axios'
 
 const Body = () => {
 
+  const [query, setQuery] = useState("")
   const [brewery, setBrewery] = useState([])
 
   const fetchBrewery = async () => {
     const getBrew = await axios.get("https://api.openbrewerydb.org/breweries")
     setBrewery(getBrew.data)
-
   }
   
-
   useEffect(() => {
    fetchBrewery()
   }, [])
 
   return (
     <div>
-      <Search props={brewery}/>
-      <br />
-      <br />
-      {brewery.map((item) => (
-        <Breweries name={item.name} brewery_type={item.brewery_type} city={item.city} id={item.id}/>
-       ) )
-      }
+      <form className="search-header">
+        <h1>Brewery App</h1>
+        <br/>
+        <input className="search-bar" placeholder="Search by entering brewery name here..." onChange={e => setQuery(e.target.value)} />
+      </form>
+        <br/>
+        <br/>
+
+        {
+          brewery.filter(item => {
+            if (query === "") {
+              return item
+            } else if (item.name.toLowerCase().includes(query.toLowerCase())){
+              return item
+            }
+        }).map((item) => (
+          <Breweries name={item.name} brewery_type={item.brewery_type} city={item.city} id={item.id} />
+        ))
+}
+
+
     </div>
   )
 }
